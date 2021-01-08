11b38cce2f5b4d445c6106ab40d8663315069871
sampayo123 <israel_sampayo@hotmail.com>
2021-01-08 01:14:54 -0400

    Is-Commit Avances empleados

---
+++ VacacionesTesisApp/VacacionesTesisApp/Server/Controllers/EmpleadosController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VacacionesTesisApp.Server.Data;
+using VacacionesTesisApp.Shared.Models;
+
+namespace VacacionesTesisApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmpleadosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Empleados
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Empleado>>> GetEmpleados()
+        {
+            return await _context.Empleados.ToListAsync();
+        }
+
+        // GET: api/Empleados/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empleado>> GetEmpleado(string id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return empleado;
+        }
+
+        // PUT: api/Empleados/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmpleado(string id, Empleado empleado)
+        {
+            if (id != empleado.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(empleado).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpleadoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Empleados
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Empleado>> PostEmpleado(Empleado empleado)
+        {
+            _context.Empleados.Add(empleado);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (EmpleadoExists(empleado.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetEmpleado", new { id = empleado.Id }, empleado);
+        }
+
+        // DELETE: api/Empleados/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Empleado>> DeleteEmpleado(string id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            _context.Empleados.Remove(empleado);
+            await _context.SaveChangesAsync();
+
+            return empleado;
+        }
+
+        private bool EmpleadoExists(string id)
+        {
+            return _context.Empleados.Any(e => e.Id == id);
+        }
+    }
+}
